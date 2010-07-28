d4a8dfa55192d1886807a71a8d220e11a3199d53
Omar Jarjur <procrastitron@users.sourceforge.net>
2010-07-28 05:32:07 +0000

    Fixed ordering of includes

--- trunk/io.c
+++ trunk/io.c
@@ -15,14 +15,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifdef NCURSES_CONSOLE
-#include <ncurses.h>
-#else
 #ifndef BARE_HARDWARE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/select.h>
+#ifdef NCURSES_CONSOLE
+#include <ncurses.h>
 #endif
 #endif
 
