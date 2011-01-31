7f6c1a0e9d04345d0226e56512579628306731f8
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-01-31 16:26:34 +0000

    Removed the use of the temporary file

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -2,8 +2,7 @@
 SOURCE=$1
 shift 1
 ARGS=$@
-./bin/desugar < ${SOURCE} > bin/temp
-./bin/compiler < bin/temp > main.c
-rm bin/temp
+./bin/desugar < ${SOURCE} | ./bin/compiler > main.c
+#./bin/desugar < ${SOURCE} | ./bin/multitask | ./bin/compiler > main.c
 #gcc *.c -DNCURSES_CONSOLE -lncurses ${ARGS}
 gcc *.c ${ARGS}
