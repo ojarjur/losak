7d620624df15dc8feee5529dba74d66771d66dae
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-02-14 05:52:37 +0000

    Removed commented-out code

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -2,7 +2,6 @@
 SOURCE=$1
 shift 1
 ARGS=$@
-#./bin/desugar < ${SOURCE} | ./bin/compiler > main.c
 ./bin/desugar < ${SOURCE} | ./bin/symbol-table | ./bin/compiler > main.c
 #./bin/desugar < ${SOURCE} | ./bin/symbol-table | ./bin/multitask | ./bin/compiler > main.c
 #gcc *.c -DNCURSES_CONSOLE -lncurses ${ARGS}
