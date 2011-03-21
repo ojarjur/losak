99c687ce1f684e3791518dab24da49e2c9406a58
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-03-21 05:04:14 +0000

    Fixed bug in tracking test counts

--- trunk/tests/runall.sh
+++ trunk/tests/runall.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Meant to be run from the root of the losak source directory.
 PASSED=0
 FAILED=0
 for FILE in `ls tests/test*.sh`; do
   ${FILE}
-  if [ $? ]; then
+  if [ "$?" == "0" ]; then
       PASSED=`expr $PASSED + 1`
   else
       FAILED=`expr $FAILED + 1`
