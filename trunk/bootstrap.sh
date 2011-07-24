62dbf6be2890d3621916cb95fe3067414f625b30
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-07-24 04:16:12 +0000

    Added the standard-library support to the bootstrapping code

--- trunk/bootstrap.sh
+++ trunk/bootstrap.sh
@@ -2,14 +2,17 @@
 if [ ! -d 'bin' ]; then mkdir bin; fi
 cp port/desugar.c main.c
 gcc *.c -o bin/desugar
-cp port/compiler.c main.c
-gcc *.c -o bin/compiler
+cp port/standard-library.c main.c
+gcc *.c -o bin/standard-library
 cp port/symbol_table.c main.c
 gcc *.c -o bin/symbol-table
 cp port/lambda-lift.c main.c
 gcc *.c -o bin/lambda-lift
-./compile.sh compiler.lsk -o bin/compile
+cp port/compiler.c main.c
+gcc *.c -o bin/compiler
 ./compile.sh desugar.lsk -o bin/desugar
+./compile.sh standard-library.lsk -o bin/standard-library
 ./compile.sh symbol-table.lsk -o bin/symbol-table
 ./compile.sh lambda-lift.lsk -o bin/lambda-lift
+./compile.sh compiler.lsk -o bin/compile
 ./compile.sh multitask.lsk -o bin/multitask
