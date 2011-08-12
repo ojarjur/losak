326a5adf67cf7034e3865be2144df4c2c1b01772
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-08-12 06:18:04 +0000

    Added test for the parse-expr function

---
+++ trunk/tests/test-read-expr.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# This is meant to be run from the root of the losak source directory.
+echo "Testing the parse-expr standard library function..."
+PROGRAM="
+(define initial-reader
+  (parse-expr (fn (expression line-number) expression)
+              (fn (error-message error-line-number) 'error)
+              1))
+(define (read string reader)
+  (cond ((atom string) reader)
+        ((function reader) (read (cdr string) (reader (car string))))
+        ('t reader)))
+(fn args
+  (let ((number-expr (read \"1234\\n\" initial-reader))
+        (symbol-expr (read \"abcd\\n\" initial-reader))
+        (null-expr (read \"()\" initial-reader))
+        (pair-expr (read \"(a b . c)\\n\" initial-reader)))
+    (if (and (= number-expr 1234)
+             (= symbol-expr 'abcd)
+             (= null-expr '())
+             (equal pair-expr '(a b . c)))
+        \"Yes\"
+        \"No\")))"
+echo ${PROGRAM} | ./compile.sh - -o bin/test-read-expr
+if [ $? ]; then
+  EXPECTED="Yes"
+  ACTUAL=$(./bin/test-read-expr)
+  if [ "$ACTUAL" = "$EXPECTED" ]; then
+    echo "\tPassed"
+    return 0
+  else
+    echo "\tFailed" "$ACTUAL"
+    return 1
+  fi
+  rm bin/test-read-expr
+else
+  echo "\tBuild Failed"
+  return 1
+fi
+
