b7e638e1ffba371d4dabb01e12a2e4a4a554c18a
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-11-19 07:22:48 +0000

    Fixed typo

--- trunk/bootstrap.sh
+++ trunk/bootstrap.sh
@@ -14,5 +14,5 @@ gcc *.c -o bin/compiler
 ./compile.sh standard-library.lsk -o bin/standard-library
 ./compile.sh symbol-table.lsk -o bin/symbol-table
 ./compile.sh lambda-lift.lsk -o bin/lambda-lift
-./compile.sh compiler.lsk -o bin/compile
+./compile.sh compiler.lsk -o bin/compiler
 ./compile.sh multitask.lsk -o bin/multitask
