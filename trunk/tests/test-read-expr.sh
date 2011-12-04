beacdd98758121453803200f4d1a3b1539ba8698
Omar Jarjur <procrastitron@users.sourceforge.net>
2011-12-04 02:46:57 +0000

    Added tests for parse errors

--- trunk/tests/test-read-expr.sh
+++ trunk/tests/test-read-expr.sh
@@ -14,13 +14,21 @@ PROGRAM="
   (let ((number-expr (read \"1234\\n\" initial-reader))
         (symbol-expr (read \"abcd\\n\" initial-reader))
         (null-expr (read \"()\" initial-reader))
-        (pair-expr (read \"(a b . c)\\n\" initial-reader)))
+        (pair-expr (read \"(a b . c)\\n\" initial-reader))
+        (unexpected-period (read \".\\n\" initial-reader))
+        (missing-tail (read \"(a .)\\n\" initial-reader))
+        (too-many-periods (read \"(a . .)\\n\" initial-reader))
+        (too-many-tails (read \"(a . b c)\\n\" initial-reader)))
     (if (and (= number-expr 1234)
              (= symbol-expr 'abcd)
              (= null-expr '())
-             (equal pair-expr '(a b . c)))
+             (equal pair-expr '(a b . c))
+             (= unexpected-period 'error)
+             (= missing-tail 'error)
+             (= too-many-periods 'error))
         \"Yes\"
-        \"No\")))"
+        \"No\"
+        )))"
 echo ${PROGRAM} | ./compile.sh - -o bin/test-read-expr
 if [ $? ]; then
   EXPECTED="Yes"
