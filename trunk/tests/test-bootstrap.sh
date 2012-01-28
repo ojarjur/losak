444dfa8bb6eb31d658a944c929ab12689fb396fc
Omar Jarjur <procrastitron@users.sourceforge.net>
2012-01-28 08:03:28 +0000

    Reduced one pass of the bootstrapping test

--- trunk/tests/test-bootstrap.sh
+++ trunk/tests/test-bootstrap.sh
@@ -2,24 +2,22 @@
 # This is meant to be run from the root of the losak source directory.
 echo "Testing bootstrapping the compiler..."
 ./bootstrap.sh
-./compile.sh compiler.lsk -o bin/compiler
-EXPECTED_COMPILER_SOURCE=`cat main.c`
 ./compile.sh desugar.lsk -o bin/desugar
-EXPECTED_DESUGAR_SOURCE=`cat main.c`
-./compile.sh symbol-table.lsk -o bin/symbol-table
-EXPECTED_SYMBOL_TABLE_SOURCE=`cat main.c`
-./compile.sh lambda-lift.lsk -o bin/lambda-lift
-EXPECTED_LAMBDA_LIFT_SOURCE=`cat main.c`
-./compile.sh compiler.lsk -o bin/compiler
-if [ "$EXPECTED_COMPILER_SOURCE" = "$(cat main.c)" ]; then
-    ./compile.sh desugar.lsk -o bin/desugar
-    if [ "$EXPECTED_DESUGAR_SOURCE" = "$(cat main.c)" ]; then
+if [ "$(cat main.c)" = "$(cat port/desugar.c)" ]; then
+    ./compile.sh standard-library.lsk -o bin/standard-library
+    if [ "$(cat main.c)" = "$(cat port/standard-library.c)" ]; then
         ./compile.sh symbol-table.lsk -o bin/symbol-table
-        if [ "$EXPECTED_SYMBOL_TABLE_SOURCE" = "$(cat main.c)" ]; then
+        if [ "$(cat main.c)" = "$(cat port/symbol-table.c)" ]; then
             ./compile.sh lambda-lift.lsk -o bin/lambda-lift
-            if [ "$EXPECTED_LAMBDA_LIFT_SOURCE" = "$(cat main.c)" ]; then
-                echo "\tPassed"
-                return 0
+            if [ "$(cat main.c)" = "$(cat port/lambda-lift.c)" ]; then
+                ./compile.sh compiler.lsk -o bin/compiler
+                if [ "$(cat main.c)" = "$(cat port/compiler.c)" ]; then
+                    echo "\tPassed"
+                    return 0
+                else
+                    echo "\tFailed: Compiler match failed."
+                    return 1
+                fi
             else
                 echo "\tFailed: Lambda-Lift match failed."
                 return 1
@@ -29,10 +27,10 @@ if [ "$EXPECTED_COMPILER_SOURCE" = "$(cat main.c)" ]; then
             return 1
         fi
     else
-        echo "\tFailed: Desugar match failed."
+        echo "\tFailed: Standard library match failed."
         return 1
     fi
 else
-    echo "\tFailed: Compiler match failed."
+    echo "\tFailed: Desugar match failed."
     return 1
 fi
