51733097ba1205733667ce9a05abc56b9f224424
Omar Jarjur <procrastitron@users.sourceforge.net>
2012-04-04 03:15:15 +0000

    Made the bootstrapping script generic so it doesn't have to be updated as compiler passes are added and removed

--- trunk/bootstrap.sh
+++ trunk/bootstrap.sh
@@ -1,22 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 if [ ! -d 'bin' ]; then mkdir bin; fi
-cp port/desugar.c main.c
-gcc *.c -o bin/desugar
-cp port/standard-library.c main.c
-gcc *.c -o bin/standard-library
-cp port/symbol-table.c main.c
-gcc *.c -o bin/symbol-table
-cp port/cps-transform.c main.c
-gcc *.c -o bin/cps-transform
-cp port/lambda-lift.c main.c
-gcc *.c -o bin/lambda-lift
-cp port/compiler.c main.c
-gcc *.c -o bin/compiler
-./compile.sh desugar.lsk -o bin/desugar
-./compile.sh standard-library.lsk -o bin/standard-library
-./compile.sh symbol-table.lsk -o bin/symbol-table
-./compile.sh cps-transform.lsk -o bin/cps-transform
-./compile.sh lambda-lift.lsk -o bin/lambda-lift
-./compile.sh compiler.lsk -o bin/compiler
-./compile.sh multitask.lsk -o bin/multitask
-./compile.sh codegen.lsk -o bin/codegen
+for FILE in `find port -name '*.c'`; do
+  SOURCE=$(echo ${FILE} | sed 's/port//' | sed 's/\.c/.lsk/')
+  TARGET=$(echo ${FILE} | sed 's/port/bin/' | sed 's/\.c//')
+  cp ${FILE} main.c
+  gcc *.c -o ${TARGET}
+done
+for SOURCE in `ls *.lsk`; do
+  if [ "${SOURCE}" != "kernel.lsk" ]; then
+   TARGET="bin/$(echo ${SOURCE} | sed 's/\.lsk//')"
+   ./compile.sh ${SOURCE} -o ${TARGET}
+  fi
+done
