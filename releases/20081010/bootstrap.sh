0ca7f4d6619d88f0968ed4636c30676e4d4dcba9
Omar Jarjur <procrastitron@users.sourceforge.net>
2013-01-05 15:56:53 -0800

    Removed the old releases directory now that we switched from svn to git

--- releases/20081010/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mzscheme -mv -f port/compiler.scm -e "(and (compile_file \"compiler.lsk\") (exit))" > main.c
-gcc *.c -o bootstrap.tmp
-./bootstrap.tmp < compiler.lsk > main.c
-gcc *.c -o compiler
-rm bootstrap.tmp
--- releases/20081010/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SOURCE=$1
-shift 1
-ARGS=$@
-./compiler < ${SOURCE} > main.c
-#gcc *.c -lncurses ${ARGS}
