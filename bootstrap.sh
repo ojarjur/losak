adbe61161c809b65263722b57fc3fc07504b1810
Omar Jarjur <procrastitron@users.sourceforge.net>
2013-05-18 15:30:33 -0700

    Fixed bug in the bootstrapping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PORT_FILES='port/compiler.c port/symbol-table.c port/cps-transform.c port/lambda-lift.c port/codegen.c'
-SOURCE_FILES='src/symbol-table.lsk src/cps-transform.lsk src/lambda-lift.lsk src/codegen.lsk'
+SOURCE_FILES='src/cps-transform.lsk src/lambda-lift.lsk src/codegen.lsk src/symbol-table.lsk'
 if [ ! -d 'bin' ]; then mkdir bin; fi
 for FILE in $(echo ${PORT_FILES}); do
   TARGET=$(echo ${FILE} | sed 's/port/bin/' | sed 's/\.c//')
@@ -9,7 +9,7 @@ done
 echo "Compiling main.lsk"
 ./compile.sh src/main.lsk -o bin/compiler || exit 1
 for SOURCE in $(echo ${SOURCE_FILES}); do
-  TARGET="bin/$(echo ${SOURCE} | sed 's/\.lsk//')"
-  echo "Compiling ${SOURCE}"
+  TARGET=$(echo ${SOURCE} | sed -e 's/\.lsk//' -e 's/src\//bin\//g')
+  echo "Compiling ${SOURCE} to ${TARGET}"
   ./compile.sh ${SOURCE} -o ${TARGET} || exit 1
 done
